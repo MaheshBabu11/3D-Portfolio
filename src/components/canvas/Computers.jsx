c858aa2f2265fb01b0dcc1898393723eedaa1ac8
MaheshBabu11 <mahesh.b.pec@gmail.com>
2023-10-16 17:08:05 +0530

    reverting the mobile changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.glb");
 
   return (
@@ -21,8 +21,8 @@ const Computers = () => {
       <pointLight intensity={5} />
       <primitive
         object={computer.scene}
-        scale={ 0.75}
-        position={[0, -4, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -4, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -67,7 +67,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers/>
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
