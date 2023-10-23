970283d83e45b45d70f9325564571b4bd6545af9
MaheshBabu11 <mahesh.b.pec@gmail.com>
2023-10-23 11:18:04 +0530

    adding a service worker to cache the assets

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('./service-worker.js') // Adjust the path to your service worker file
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
