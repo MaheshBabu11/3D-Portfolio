1db17dd21cf3968fa63207b3ad3f892efba34590
MaheshBabu11 <mahesh.b.pec@gmail.com>
2023-10-23 11:51:45 +0530

    caching policy

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,jpg,png}']
+      },
       devOptions: {
         enabled: true,
       },
