1a9bdeeda118155043600661f878daa8034027cc
MaheshBabu11 <mahesh.b.pec@gmail.com>
2023-10-23 10:48:32 +0530

    re added the lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
 import { BrowserRouter } from "react-router-dom";
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-  StarsCanvas,
-} from "./components";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import LazyLoadComponent from "./components/LazyLoadComponent";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className="relative z-0">
+        <LazyLoadComponent>
+          <About />
+        </LazyLoadComponent>
+        <LazyLoadComponent>
+          <Experience />
+        </LazyLoadComponent>
+        <LazyLoadComponent>
+          <Tech />
+        </LazyLoadComponent>
+        <LazyLoadComponent>
+          <Works />
+        </LazyLoadComponent>
+        <LazyLoadComponent>
+          <Feedbacks />
+        </LazyLoadComponent>
+        
+         
+        <div className='relative z-0'>
+        <LazyLoadComponent>
           <Contact />
+          </LazyLoadComponent>
+          <LazyLoadComponent>
           <StarsCanvas />
+          </LazyLoadComponent>
         </div>
       </div>
     </BrowserRouter>
