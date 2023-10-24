3242b91e41341404f4d9eb0e1130aee24f4f60a6
MaheshBabu11 <mahesh.b.pec@gmail.com>
2023-10-24 13:14:52 +0530

    adding back lazyloading

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,40 @@ import {
   Works,
   StarsCanvas,
 } from "./components";
+import LazyLoadComponent from "./components/LazyLoadComponent";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
+          <LazyLoadComponent><Navbar/></LazyLoadComponent>
+          <LazyLoadComponent><Hero/></LazyLoadComponent>
         </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
+        <LazyLoadComponent>
+          <About />
+        </LazyLoadComponent>
+        <LazyLoadComponent>
+          <Experience />
+        </LazyLoadComponent>
+        <LazyLoadComponent>
+          <Tech />
+        </LazyLoadComponent>
+        <LazyLoadComponent>
+          <Works />
+        </LazyLoadComponent>
+
+        <LazyLoadComponent>
+          <Feedbacks />
+        </LazyLoadComponent>
 
         <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
+          <LazyLoadComponent>
+            <Contact />
+          </LazyLoadComponent>
+          <LazyLoadComponent>
+            <StarsCanvas />
+          </LazyLoadComponent>
         </div>
       </div>
     </BrowserRouter>
